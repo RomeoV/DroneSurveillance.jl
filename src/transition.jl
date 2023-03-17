c8b01833ea8a36b7bc8f319352e41c811d47cc27
Romeo Valentin <romeov@stanford.edu>
2023-03-17 03:47:21 -0700

    Remove some redundant prediction function

--- src/transition.jl
+++ src/transition.jl
@@ -45,7 +45,6 @@ function prune_states(sc::SparseCat, ϵ_prune)
     SparseCat(sc.vals[idx], normalize(sc.probs[idx], 1))
 end
 
-# TODO maybe move this to the other project
 function predict(model::DSLinModel, s::DSState, a::DSPos; ϵ_prune=1e-4, T=1.0)
     nx, ny = size.([model.θ_Δx, model.θ_Δy], 1) .÷ 2
     states_Δx, states_Δy = (-nx:nx, -ny:ny) .|> collect
@@ -62,53 +61,12 @@ function predict(model::DSLinModel, s::DSState, a::DSPos; ϵ_prune=1e-4, T=1.0)
             prune_states(SparseCat(states_Δy, probs_Δy), ϵ_prune))
 end
 
-function predict(cal_model::DSLinCalModel, s::DSState, a::DSPos; ϵ_prune=1e-4)
-    lin_model = cal_model.lin_model
-    T = cal_model.temperature
-
-    nx, ny = size.([lin_model.θ_Δx, lin_model.θ_Δy], 1) .÷ 2
-    states_Δx, states_Δy = (-nx:nx, -ny:ny) .|> collect
-
-    Δx = s.agent.x - s.quad.x
-    Δy = s.agent.y - s.quad.y
-    ξ = [Δx, Δy, a.x, a.y, 1]
-    softmax(x) = exp.(x / T) / sum(exp.(x / T))
-    probs_Δx, probs_Δy = (softmax(lin_model.θ_Δx * ξ),
-                          softmax(lin_model.θ_Δy * ξ))
-
-    # we prune states with small probability
-    return (prune_states(SparseCat(states_Δx, probs_Δx), ϵ_prune),
-            prune_states(SparseCat(states_Δy, probs_Δy), ϵ_prune))
-end
+predict(cal_model::DSLinCalModel, s::DSState, a::DSPos; ϵ_prune=1e-4) =
+    predict(cal_model.lin_model, s, a; ϵ_prune=ϵ_prune, T=cal_model.T)
 
 # make a prediction set with the linear model
-function predict(model::DSLinModel, s::DSState, a::DSPos, λ::Real; ϵ_prune=1e-4)
+function predict(model::Union{DSLinModel, DSLinCalModel}, s::DSState, a::DSPos, λ::Real; ϵ_prune=1e-4)
     lhs_distr, rhs_distr = predict(model, s, a; ϵ_prune=ϵ_prune)
-
-    # Shuffle predictions, keep adding to prediction set until just over or just under
-    # desired probability (whichever has smaller "gap" to λ).
-    lhs_pred_set, rhs_pred_set = Tuple([begin
-            perm = shuffle(eachindex(distr.probs))
-            p_perm = distr.probs[perm]
-            p_cum = cumsum(p_perm)
-
-            idx = begin
-                idx = findfirst(>=(λ), p_cum)
-                gap_hi = p_cum[idx] - λ
-                gap_lo = λ - get(p_cum, idx-1, 0)
-                (gap_hi < gap_lo ? idx : idx-1)
-            end
-
-            val_perm = distr.vals[perm]
-            Set(val_perm[1:idx])
-        end
-        for distr in [lhs_distr, rhs_distr]
-    ])
-    return lhs_pred_set, rhs_pred_set
-end
-
-function predict(cal_model::DSLinCalModel, s::DSState, a::DSPos, λ::Real; ϵ_prune=1e-4)
-    lhs_distr, rhs_distr = predict(cal_model, s, a; ϵ_prune=ϵ_prune)
 
     # Shuffle predictions, keep adding to prediction set until just over or just under
     # desired probability (whichever has smaller "gap" to λ).
@@ -151,7 +109,6 @@ function predict(conf_model::DSConformalizedModel, s::DSState, a::DSPos, λ::Rea
     pred_set_Δy = states[2][idx_Δy] |> Set
     return (pred_set_Δx, pred_set_Δy)
 end
-
 
 function POMDPs.transition(mdp::DroneSurveillanceMDP, s::DSState, a::DSPos) :: Union{Deterministic, SparseCat}
     return transition(mdp, mdp.agent_strategy, mdp.transition_model, s, a)
